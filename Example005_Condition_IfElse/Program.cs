36610417d74b9341a3781b03dc03898165109fbb
Anton <anton_svirsky@mail.ru>
2022-06-21 22:40:24 +0300

    add Example005_Condition_IfElse

---
+++ Example005_Condition_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "нася")
+{
+    Console.WriteLine("Ура, это же Нася!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
